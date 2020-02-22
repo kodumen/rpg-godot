c8a0281a0f4b024b0a0d9a1c3360251c8045bf1b
kodumen <r.b.daitan@gmail.com>
2020-02-22 09:39:13 +0800

    WIP

--- scripts/ActionItem.gd
+++ scripts/ActionItem.gd
@@ -1,15 +1,43 @@
 extends HBoxContainer
 
+
 export var text: String
+
+
+onready var indicator = $FocusIndicator
+var interacted = false
+
 
 func _ready():
 	$Label.text = text
-	$FocusIndicator.modulate = Color(1, 1, 1, 0)
+	highlight(false)
+
+
+func _process(delta):
+	if pressed() and not interacted:
+		interacted = true
+		print(text)
+	
 
 
 func _on_ActionItem_focus_entered():
-	$FocusIndicator.modulate = Color(1, 1, 1, 1)
+	highlight(true)
+	ControlManager.focus_on(self)
+	interacted = false
 
 
 func _on_ActionItem_focus_exited():
-	$FocusIndicator.modulate = Color(1, 1, 1, 0)
+	highlight(false)
+	interacted = false
+
+
+func highlight(is_highlighted: bool):
+	if is_highlighted:
+		indicator.modulate.a = 1
+	else:
+		indicator.modulate.a = 0
+		
+	
+func pressed() -> bool:
+	return (ControlManager.is_focused(self)
+		and Input.is_action_pressed("interact"))
--- scripts/ActionMenu.gd
+++ scripts/ActionMenu.gd
@@ -1,4 +1,12 @@
 extends PanelContainer
 
 func _ready():
-	$VBoxContainer/Attack.grab_focus()
+	BattleManager.connect("started", self, "focus")
+	
+
+func focus():
+	var items = $VBoxContainer
+	if items.get_children().empty():
+		return
+	
+	items.get_children()[0].grab_focus()
